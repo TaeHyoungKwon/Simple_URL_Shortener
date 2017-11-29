6aeddc4d81946d02148b228c3c48ba4ee71f4ebb
TaeHyoungKwon <kwon5604@naver.com>
2017-11-30 01:16:26 +0900

    Create App

--- src/shortener/models.py
+++ src/shortener/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class ShortenURL(models.Model):
+    origin_url = models.CharField(max_length=200)
+    additional_url = models.CharField(max_length=8)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
