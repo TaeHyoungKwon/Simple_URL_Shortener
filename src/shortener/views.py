dc61235e1b99c002b42ebba24e24c1bc6ca1d41e
Hoorooroob <kth5604@gmail.com>
2019-01-24 07:48:08 +0900

    [Refactor] README 수정 과정에서 발견한 것들 리팩토링 작업

--- src/shortener/views.py
+++ src/shortener/views.py
@@ -17,62 +17,66 @@ def shortener_home(request):
     if request.method == 'POST':
         form = CreateShortenURLForm(request.POST or None)
 
-        #Confirm form object is valid or not
-        if form.is_valid(): 
+        # Confirm form object is valid or not
+        if form.is_valid():
+
             new_url = form.cleaned_data.get("origin_url")
             confirm = ShortenURL.objects.filter(origin_url=new_url)
 
-            #if duplicated url exists, redirect that url
+            # if duplicated url exists, redirect that url
             if confirm.exists():
                 instance = ShortenURL.objects.get(origin_url=new_url)
-                return HttpResponseRedirect(instance.get_absolute_url())            
-            
-            #else Create New shortened URL
-            else:
-                owner = request.user
-                instance, created = ShortenURL.objects.get_or_create(origin_url=new_url, owner=owner)
-                context = {
-                    "instance" : instance,
-                    "created":created,
-                }
+                print("instance1: {}".format(instance))
                 return HttpResponseRedirect(instance.get_absolute_url())
 
-        #If form is not valid,
+            # else Create New shortened URL
+            else:
+
+                instance, created = ShortenURL.objects.get_or_create(
+                    origin_url=new_url)
+                context = {
+                    "instance": instance,
+                    "created": created,
+                }
+                print("instance2: {}".format(instance))
+                return HttpResponseRedirect(instance.get_absolute_url())
+
+        # If form is not valid,
         else:
-            context ={
-                "form":form
+            context = {
+                "form": form
             }
-            return render(request,'shortener/home.html',context)
+            return render(request, 'shortener/home.html', context)
 
     # If request method is "GET",
-    else: 
+    else:
         form = CreateShortenURLForm()
-        context = {"form":form,}
+        context = {"form": form, }
 
-    return render(request,'shortener/home.html',context)
+    return render(request, 'shortener/home.html', context)
 
 
-
-def shortener_detail(request,additional_url):
+def shortener_detail(request, additional_url):
     '''
     * Detail page about shortened url
         - Return detail.html templates many information about specific URL
     '''
-    instance = get_object_or_404(ShortenURL,additional_url=additional_url)
-    information, created = Information.objects.get_or_create(shorten_url=instance)
-    qs = HitUpdatedTime.objects.all().filter(information=information).order_by("-updated_at")[:5]
+    instance = get_object_or_404(ShortenURL, additional_url=additional_url)
+    information, created = Information.objects.get_or_create(
+        shorten_url=instance)
+    qs = HitUpdatedTime.objects.all().filter(
+        information=information).order_by("-updated_at")[:5]
 
     created_at = instance.created_at.strftime('%b %d, %Y')
 
     context = {
-        'instance':instance,
-        'hit_date':qs,
-        'information':information,
-        'created_at' : created_at,
+        'instance': instance,
+        'hit_date': qs,
+        'information': information,
+        'created_at': created_at,
     }
-    return render(request,'shortener/detail.html',context)
+    return render(request, 'shortener/detail.html', context)
 
-    
 
 def redirect_origin_url(request, additional_url):
     '''
@@ -97,8 +101,7 @@ def analysis(request):
     '''
     * Analysis page about hit count for URL
     '''
-    return render(request,"shortener/analysis.html",{})
-
+    return render(request, "shortener/analysis.html", {})
 
 
 class ChartData(APIView):
@@ -109,23 +112,23 @@ class ChartData(APIView):
     permission_classes = []
 
     def get(self, request, format=None):
-        
+
         default_items = []
 
         qs_url = ShortenURL.objects.all()
-        labels = [ str(obj.origin_url) for obj in qs_url]
+        labels = [str(obj.origin_url) for obj in qs_url]
 
         for url in labels:
-            qs_cnt = Information.objects.get(shorten_url__origin_url= url)
+            qs_cnt = Information.objects.get(shorten_url__origin_url=url)
             default_items.append(qs_cnt.hit)
-  
-        data={
-        "labels":labels,
-        'default':default_items,
-            }
+
+        data = {
+            "labels": labels,
+            'default': default_items,
+        }
         return Response(data)
 
 
-#Generic ListView about Shortened URL
-url_list = ListView.as_view(model=ShortenURL,queryset=ShortenURL.objects.filter(is_public=True).order_by('-created_at'))
-
+# Generic ListView about Shortened URL
+url_list = ListView.as_view(model=ShortenURL, queryset=ShortenURL.objects.filter(
+    is_public=True).order_by('-created_at'))
